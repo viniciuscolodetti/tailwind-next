2ac51728d725928a41b31b6492b40eebebbe1f28
Vinicius Colodetti <vinicius.colodetti@gmail.com>
2023-08-17 20:02:04 -0300

    Setup inicial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,13 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.tsx',
   ],
   theme: {
     extend: {
+      colors: {
+        diego: '#8257e6',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
