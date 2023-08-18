7e413b344d90c9967d6f5a2b0be46c1c1a265c26
Vinicius Colodetti <vinicius.colodetti@gmail.com>
2023-08-17 21:43:46 -0300

    Sidebar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/**/*.tsx',
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+        profile: 'max-content 1fr min-content',
+      },
+      maxWidth: {
+        app: '700px',
+      },
       colors: {
         diego: '#8257e6',
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
